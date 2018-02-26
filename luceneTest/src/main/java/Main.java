58bb0f2212894f2cc6e5d2e0772c42d355c33155
macjiong <760783896@qq.com>
2018-02-26 20:03:40 +0000

    update code
    update code

--- luceneTest/src/main/java/Main.java
+++ luceneTest/src/main/java/Main.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         try{
             Indexer indexer = new Indexer();
-            indexer.createIndex();
+            indexer.createIndex(1);
             Searcher searcher = new Searcher();
             searcher.readIndex(1);
             ArrayList<ArrayList<Integer>> base = FiledataReader.readQrelScore();
@@ -13,8 +12,8 @@ public class Main {
             Evaluator evaluator = new Evaluator();
             double averageMap = evaluator.avgMAP(searcher,base,queries);
             double averageRecall = evaluator.avgRecall(searcher,base,queries);
-            System.out.println("MAP of BM25 is: "+averageMap);
-            System.out.println("Recall of BM25 is: "+averageRecall);
+            System.out.println("Standardanalyzer MAP of BM25 is: "+averageMap);
+            System.out.println("Standardanalyzer Recall of BM25 is: "+averageRecall);
 
             Searcher searcher1 = new Searcher();
             searcher1.readIndex(2);
@@ -23,8 +22,30 @@ public class Main {
             Evaluator evaluator1 = new Evaluator();
             double averageMap1 = evaluator1.avgMAP(searcher1,base1,queries1);
             double averageRecall1 = evaluator1.avgRecall(searcher1,base1,queries1);
-            System.out.println("MAP of TF-IDF is: "+averageMap1);
-            System.out.println("Recall of TF-IDF is: "+averageRecall1);
+            System.out.println("Standardanalyzer MAP of TF-IDF is: "+averageMap1);
+            System.out.println("Standardanalyzer Recall of TF-IDF is: "+averageRecall1);
+
+            Indexer stopindexer = new Indexer();
+            stopindexer.createIndex(2);
+            Searcher stopsearcher = new Searcher();
+            stopsearcher.readIndex(1);
+            ArrayList<ArrayList<Integer>> stopbase = FiledataReader.readQrelScore();
+            ArrayList<String> stopqueries =  FiledataReader.readQueries();
+            Evaluator stopevaluator = new Evaluator();
+            double stopaverageMap = stopevaluator.avgMAP(stopsearcher,stopbase,stopqueries);
+            double stopaverageRecall = stopevaluator.avgRecall(stopsearcher,stopbase,stopqueries);
+            System.out.println("Stopanalyzer MAP of BM25 is: "+stopaverageMap);
+            System.out.println("Stopanalyzer Recall of BM25 is: "+stopaverageRecall);
+
+            Searcher stopsearcher1 = new Searcher();
+            stopsearcher1.readIndex(2);
+            ArrayList<ArrayList<Integer>> stopbase1 = FiledataReader.readQrelScore();
+            ArrayList<String> stopqueries1 =  FiledataReader.readQueries();
+            Evaluator stopevaluator1 = new Evaluator();
+            double stopaverageMap1 = stopevaluator1.avgMAP(stopsearcher1,stopbase1,stopqueries1);
+            double stopaverageRecall1 = stopevaluator1.avgRecall(stopsearcher1,stopbase1,stopqueries1);
+            System.out.println("Stopanalyzer MAP of TF-IDF is: "+stopaverageMap1);
+            System.out.println("Stopanalyzer Recall of TF-IDF is: "+stopaverageRecall1);
         }catch (Exception e){
             e.printStackTrace();
             System.exit(1);
